78fd5e1ac5fa08de37ba80b3c63a5826ef87df4c
vasiliipm <duffman085@gmail.com>
2025-03-23 17:46:27 +0300

    Initial React frontend commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/message")  // Пока локально
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+      .catch(err => console.error("Ошибка:", err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Привет, мир!</h1>
+      <p>Ответ от бэкенда: {message}</p>
     </div>
   );
 }
